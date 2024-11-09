605cd3b61e6d396b6d30bb6e0f379f3bc8ad6d0b
Hassan Abdullah <hassan.satti200@gmail.com>
2024-11-08 20:48:20 -0800

    Hello

---
+++ Exercise 09/Hello.py
@@ -0,0 +1,26 @@
+## Exercise 9: Hello - 10 Marks
+
+### Fill in the blanks in the code below so that the function hello() prints "Hello" to the console.
+
+
+### def hello():
+                 # Fill in this blank to print "Hello" to the console
+
+### def main():
+                  # Fill in this blank to call the hello() function
+
+### if __name__ == "__main__":
+###     main()
+
+
+
+
+
+def hello():
+    print("Hello")                             ## Prints "Hello" to the console
+
+def main():
+    hello()                                    ## Calls the hello() function
+
+if __name__ == "__main__":                      
+    main()
